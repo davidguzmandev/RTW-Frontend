0c815e08cdca209a636a12b9b78cf4856c433066
David Guzman <daguz.91@gmail.com>
2024-11-11 13:47:06 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../partials/Navbar'
 import { UserContext } from '../utils/UserContext';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   const navigate = useNavigate();
