c3ccdc0d4db1ca43c14a93363b366446fe2031cc
davidruncode <daguz.91@gmail.com>
2024-11-17 13:33:57 -0500

    UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,11 @@ const Home = () => {
                         <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
                             <div className="text-center mb-10">
                                 <h1 className="font-bold text-3xl text-gray-900">Sign In</h1>
-                                <p>Enter your information to Sign In</p>
+                                <p>Enter your information to Sign In</p><br />
+                                <p>-- Dear guest, login with: --</p>
+                                <p><strong>Email:</strong> admin@gmail.com</p>
+                                <p><strong>Password:</strong> 123</p>
+                                <p>and wait 20 second</p>
                             </div>
                                 <SignIn/>
                             </div>
--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className='text-center text-gray-400 text-sm'>
       <p>Version 1.0</p>
-      <p>© 2024 Made with ♥ por David Guzman</p>
+      <p>© 2024 Made with ♥ by David Guzman</p>
     </div>
   )
 }
